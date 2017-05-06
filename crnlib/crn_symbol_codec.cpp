b095036fe2a4b66a58b075b9c1440b317bec3c37
Anonymous Maarten <anonymous.maarten@gmail.com>
2017-05-06 03:38:23 +0200

    Fix warning: statement has no effect [-Wunused-value]

--- crnlib/crn_symbol_codec.cpp
+++ crnlib/crn_symbol_codec.cpp
@@ -221,7 +221,7 @@ namespace crnlib
          status = prefix_coding::generate_decoder_tables(m_total_syms, &m_code_sizes[0], m_pDecode_tables, m_decoder_table_bits);
 
       CRNLIB_ASSERT(status);
-      status;
+      (void)status;
 
       m_update_cycle = (5 * m_update_cycle) >> 2;
       uint max_cycle = (m_total_syms + 6) << 3; // this was << 2 - which is ~12% slower but compresses around .5% better
@@ -554,7 +554,7 @@ namespace crnlib
 
    void adaptive_arith_data_model::init(bool encoding, uint total_syms)
    {
-      encoding;
+      (void)encoding;
       if (!total_syms)
       {
          clear();
